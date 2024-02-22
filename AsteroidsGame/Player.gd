e2808fd03added11d5c2e24207594f822f735fd2
Austin Bell <144272907+Aust1nB311@users.noreply.github.com>
2024-02-22 13:06:24 -0600

    Updated Files 1:06 PM, 2/22/24

--- AsteroidsGame/Player.gd
+++ AsteroidsGame/Player.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed = 300
 var screen_size
+var angular_speed = PI
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.RIGHT.rotated(rotation) * speed
+		
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2.LEFT.rotated(rotation) * speed
+	
+	if velocity != 0:
+		$AnimatedSprite2D.animation = "flying"
+
+	position += velocity * delta
